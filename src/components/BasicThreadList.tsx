13d546d5013dfb24aa10216c484fcff701694cb7
Soumyadip <soumyadip1107@gmail.com>
2023-12-19 18:07:56 +0800

    Added login component

--- src/components/BasicThreadList.tsx
+++ src/components/BasicThreadList.tsx
@@ -1,4 +1,5 @@
 import "../App.css";
+import { Login } from "./Login";
 
 import React from "react";
 import { Link } from "react-router-dom";
@@ -6,7 +7,8 @@ import { Link } from "react-router-dom";
 const BasicThreadList: React.FC = () => {
     return (
         <div style={{ width: "25vw", margin: "auto", textAlign: "center" }}>
-            <h4>{"Welcome to my forum!"}</h4>
+            <h4>{"Welcome to ForumHub!"}</h4>
+            <Login />
             <ul>
                 <li>
                     <Link to="/thread/1">{"Inspirational Quotes"}</Link>
